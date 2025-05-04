17875c4668b3141baff7016852a680920f3656c0
Danilo Silva <danilo.silva@msn.com>
2025-05-04 15:35:40 -0300

    feat: integrate Firebase into Angular application
    - Added Firebase and AngularFire dependencies in package.json
    - Configured Firebase initialization in app.config.ts
    - Updated main.ts for improved error handling during bootstrap
    - Created environment.ts for Firebase configuration

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAozEaCtv0Z7PQYwXi6LJ-AKHhEdOBf5Tk',
+    authDomain: 'instituto-virtus-app.firebaseapp.com',
+    projectId: 'instituto-virtus-app',
+    storageBucket: 'instituto-virtus-app.firebasestorage.app',
+    messagingSenderId: '643815073025',
+    appId: '1:643815073025:web:bb15c3fc571841e9d7a700',
+  },
+};
